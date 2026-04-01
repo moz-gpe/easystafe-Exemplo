1e81c36f6e664d4bcc0ce493ef436fbe6e40756a
Joe Lara <joseph.lara@gmail.com>
2026-04-01 09:43:49 +0200

    clear ascii part 2

--- Scripts/pdf_extraction.R
+++ Scripts/pdf_extraction.R
@@ -1,12 +1,11 @@
 library(tidyverse)
 library(pdftools)
 library(openxlsx)
-
 library(easystafe)
 #source("Scripts/functions.R")
 
 
-path_folder_source <- "~/Data/esistafe/razao_contabilistico/"
+path_folder_source <- "Data/razao_cont/2026_02/"
 
 
 df_razao <- processar_extracto_razao_c(
@@ -17,3 +16,13 @@ df_razao <- processar_extracto_razao_c(
 gravar_extracto_razao_c(df_razao)
 
 
+
+
+# TESTING -----------------------------------------------------------------
+
+path_folder_source_absa <- "Data/razao_cont/2026_02/outro/"
+
+df_absa <- processar_extracto_absa(path_folder_source_absa)
+
+
+gravar_extracto_absa(df_absa)
