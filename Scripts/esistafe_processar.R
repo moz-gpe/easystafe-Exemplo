400adc9b05779da7489bcdb5c30957a50c1e5b86
Joe Lara <joseph.lara@gmail.com>
2026-05-14 08:36:31 +0200

    clean up esistafe_processar script

--- Scripts/esistafe_processar.R
+++ Scripts/esistafe_processar.R
@@ -1,4 +1,4 @@
-# pak::pak("moz-gpe/easystafe")
+pak::pak("moz-gpe/easystafe")
 # https://github.com/moz-gpe/easystafe
 
 library(tidyverse)
@@ -11,88 +11,90 @@ library(easystafe)
 
 # GLOBAL VARS -------------------------------------------------
 
-metadata_lookup <- "Documents/lookup_ugb.xlsx"
+metadata_lookup <- "Documents/lookup.xlsx"
+
+paths_esistafe <- c(
+  "Data/2025/",
+  "Data/2026/"
+)
+
+#path_razao_contabalistica <- "Data/razao_cont/2026_01/"
 
 
 # LOAD LOOKUPS ------------------------------------------------------------
 
+lookups <- list(
 
-lookup_ugb <- suppressMessages(
-  read_excel(metadata_lookup, sheet = "Sheet1")) %>%
-  clean_names() %>%
-  select(codigo_ugb,
-         provincia,
-         distrito,
-         ambito,
-         starts_with("adm"),
-         nivel_da_instituicao,
-         descricao) %>%
-  filter(!codigo_ugb == "Total")
+  ugb = suppressMessages(
+    read_excel(metadata_lookup, sheet = "ugb")) %>%
+    clean_names() %>%
+    select(codigo_ugb,
+           provincia,
+           distrito,
+           ambito,
+           starts_with("adm"),
+           nivel_da_instituicao,
+           descricao) %>%
+    filter(!codigo_ugb == "Total"),
+
+  funcao = suppressMessages(
+    read_excel(metadata_lookup, sheet = "metadados")) %>%
+    clean_names() %>%
+    select(funcao,
+           funcao_nivel = classificacao_funcional_por_nivel) %>%
+    filter(!is.na(funcao)),
+
+  programa = suppressMessages(
+    read_excel(metadata_lookup, sheet = "programa_tipo")) %>%
+    clean_names() %>%
+    select(programa,
+           programa_tipo) %>%
+    filter(!is.na(programa_tipo))
+)
 
 
-lookup_funcao <- suppressMessages(
-  read_excel(metadata_lookup, sheet = "Sheet2")) %>%
-  clean_names() %>%
-  select(funcao,
-         funcao_nivel = classificacao_funcional_por_nivel) %>%
-  filter(!is.na(funcao))
+# PROCESSAR E GRAVAR E-SISTAFE -----------------------------------------------------
 
-
-lookup_programa <- suppressMessages(
-  read_excel(metadata_lookup, sheet = "Sheet2")) %>%
-  clean_names() %>%
-  select(programa_esistafe = programa_e_sistafe,
-         programa_educacao) %>%
-  filter(!is.na(programa_esistafe))
-
-
-# PROCESSAMENTO E-SISTAFE -----------------------------------------------------
-
-df_esistafe <- processar_extracto_esistafe(
-  source_path = "Data/2026/",
-  df_ugb_lookup  = lookup_ugb,
-  include_percent = FALSE,
-  include_file_metadata = TRUE,
-  include_metrica = TRUE,
-  quiet = FALSE) %>%
-  left_join(lookup_ugb, by = join_by(ugb_id == codigo_ugb)) %>%
-  left_join(lookup_funcao, by = join_by(funcao == funcao)) %>%
-  recode_programa_tipo() %>%
+df_esistafe <- paths_esistafe %>%
+  map(\(path) processar_extracto_esistafe(
+    source_path = path,
+    df_ugb_lookup          = lookups$ugb,
+    include_percent        = FALSE,
+    include_file_metadata  = TRUE,
+    include_metrica        = TRUE,
+    correct_negatives      = TRUE,
+    quiet                  = FALSE
+  )) %>%
+  bind_rows() %>%
+  left_join(lookups$ugb, by = join_by(ugb_id == codigo_ugb)) %>%
+  left_join(lookups$funcao, by = join_by(funcao == funcao)) %>%
+  left_join(lookups$programa, by = join_by(programa == programa)) %>%
   relocate(funcao_nivel, .after = funcao) %>%
   relocate(provincia, distrito, ambito, adm2020_24, adm2025_29,
            nivel_da_instituicao, descricao, programa_tipo,
            .after = ced)
 
+gravar_esistafe(df_esistafe,
+                output_folder = "Dataout/",
+                quiet = TRUE)
 
-# PROCESSAMENTO RAZAO CONT. & ABSA ---------------------------------------------------------
 
-path_folder_source <- "Data/razao_cont/2026_01/"
+# PROCESSAR E GRAVAR RAZAO CONTABILISTICA. & ABSA ---------------------------------------------------------
 
-df_razao <- processar_extracto_razao_c(source_path = path_folder_source)
-df_absa <- processar_extracto_absa(path_folder_source)
+df_razao <- processar_extracto_razao_c(source_path = path_razao_contabalistica)
+df_absa <- processar_extracto_absa(source_path = path_razao_contabalistica)
 
 df_razao <- bind_rows(df_razao, df_absa)
 
-# GRAVAR FICHEIRO DO PERIODO -----------------------------------------------------------------
-
-gravar_extracto_sistafe(df_esistafe)
 gravar_extracto_razao_c(df_razao)
 
-
-rm(df_absa,
-   lookup_funcao,
-   lookup_programa,
-   lookup_ugb)
-
+rm(df_absa)
 
 # COMPILAR FICHEIROS ------------------------------------------------------
 
-path_sistafe <- gravar_compilacao_sistafe()
-read_xlsx(path_sistafe) |>
-  write_parquet(sub("\\.xlsx$", ".parquet", path_sistafe),
-                compression = "zstd", compression_level = 9)
 
 path_razao <- gravar_compilacao_razao_c()
 read_xlsx(path_razao) |>
   write_parquet(sub("\\.xlsx$", ".parquet", path_razao),
                 compression = "zstd", compression_level = 9)
+
